eb8d6ab63c076c0e85165934249640028e8b9ea0
keithdh <keith.holmes@intoview.co>
2015-01-28 17:03:45 +0000

    adding files

---
+++ script.js
@@ -0,0 +1,4 @@
+var p = showModalDialog("14.04.multiprompt.html",
+                          ["Enter 3D point coordinates", "x", "y", "z"],
+                          "dialogwidth:400; dialogheight:300; resizable:yes");  
+
